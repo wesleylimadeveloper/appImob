462089ee51e0e01dc3c975d13651c18addd5e861
Wesley Lima <wesley.lima@invitatech.com.br>
2024-03-19 17:00:57 -0300

    Desenvolvimento do interceptor para adicionar o token de autenticação em todas as requisições, quando disponível

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,24 @@
 import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const api = axios.create({
   baseURL: "https://api.imobiliariasistema.com.br/api/",
 });
+
+api.interceptors.request.use(
+  async (config) => {
+    const userStorage = await AsyncStorage.getItem("@appImob:auth_token");
+
+    if (userStorage) {
+      const user = JSON.parse(userStorage);
+      config.headers.Authorization = `Bearer ${user.token}`;
+    }
+
+    return config;
+  },
+  async function (error) {
+    return Promise.reject(error);
+  }
+);
 
 export default api;
