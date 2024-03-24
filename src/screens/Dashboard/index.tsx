92e72e28381f1028f743c2a4b391b80964a03365
Wesley Lima <wesley.lima@invitatech.com.br>
2024-03-23 21:27:04 -0300

    Desenvolvimento do cabeçalho na tela de dashboard, exibindo o nome do usuário logado

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -2,10 +2,25 @@ import React, { useCallback } from "react";
 import { Ionicons } from "@expo/vector-icons";
 import { useFocusEffect, useNavigation } from "@react-navigation/native";
 
-import { Container, Text } from "./styles";
+import { useAuth } from "../../hooks/useAuth";
+
+import {
+  Container,
+  Header,
+  Avatar,
+  InitialLetter,
+  Greetings,
+  Title,
+  Subtitle,
+} from "./styles";
 
 export function Dashboard() {
+  const { userData } = useAuth();
+  const { usuario_nome } = userData;
+
   const navigation = useNavigation();
+
+  const initialLetter = usuario_nome[0].toUpperCase();
 
   useFocusEffect(
     useCallback(() => {
@@ -20,7 +35,17 @@ export function Dashboard() {
 
   return (
     <Container>
-      <Text>Dashboard</Text>
+      <Header>
+        <Avatar>
+          <InitialLetter>{initialLetter}</InitialLetter>
+        </Avatar>
+
+        <Greetings>
+          <Title>Olá, {usuario_nome}</Title>
+
+          <Subtitle>Bem-vindo(a) ao appImob</Subtitle>
+        </Greetings>
+      </Header>
     </Container>
   );
 }
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,13 +1,44 @@
 import styled from "styled-components/native";
+import { RFValue } from "react-native-responsive-fontsize";
 
 export const Container = styled.View`
-  align-items: center;
   background-color: ${({ theme }) => theme.colors.light};
   flex: 1;
-  justify-content: center;
 `;
 
-export const Text = styled.Text`
+export const Header = styled.View`
+  align-items: center;
+  flex-direction: row;
+  padding: ${RFValue(32)}px ${RFValue(16)}px;
+`;
+
+export const Avatar = styled.View`
+  align-items: center;
+  background-color: ${({ theme }) => theme.colors.secondary};
+  border-radius: 256px;
+  height: ${RFValue(64)}px;
+  justify-content: center;
+  width: ${RFValue(64)}px;
+`;
+
+export const InitialLetter = styled.Text`
+  color: ${({ theme }) => theme.colors.light};
   font-family: ${({ theme }) => theme.fonts.title};
-  font-size: ${({ theme }) => theme.fontSize.normal}px;
+  font-size: ${({ theme }) => theme.fontSize.largest}px;
+`;
+
+export const Greetings = styled.View`
+  flex: 1;
+  margin-left: 16px;
+`;
+
+export const Title = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.title};
+  font-size: ${({ theme }) => theme.fontSize.largest}px;
+`;
+
+export const Subtitle = styled.Text`
+  color: ${({ theme }) => theme.colors.primary};
+  font-family: ${({ theme }) => theme.fonts.title};
+  font-size: ${({ theme }) => theme.fontSize.larger}px;
 `;
