fb810111f38da8ffdcfdec71c95f68c6c1efb9bb
wesleylimainvita <wesleyguedes7@gmail.com>
2024-04-26 15:21:12 -0300

    Refatoração da configuração da URL da API para utilizar variáveis de ambiente do arquivo .env

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const api = axios.create({
-  baseURL: "https://api.imobiliariasistema.com.br/api/",
+  baseURL: process.env.EXPO_PUBLIC_API_URL,
 });
 
 api.interceptors.request.use(
